9b9b96da73c5418b631732629dff3abf47852bf5
Hebe3834 <hebeluna00@gmail.com>
2022-03-08 09:26:34 -0500

    anmovie

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -5,8 +5,34 @@
 
 var c = document.getElementById("main_game");
 var ctx = c.getContext("2d");
+var requestID;  //init global var for use with animation frames
+var obstacles = [[200, 0]];
+var radius = 1;
 
-ctx.fillStyle = "red"
+var clear = (e) => {
+    console.log("clear invoked...")
+    ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
+};
+
+var playGame = () => {
+  console.log("playGame invoked...")
+
+    window.cancelAnimationFrame(requestID);
+    requestID = window.requestAnimationFrame(playGame);
+
+    clear();
+    ctx.beginPath();
+
+    // for(var i = 0; i < obstacles.length; i++) {
+    //
+    // }
+    radius += 1;
+    ctx.arc(c.clientWidth/2, c.clientHeight/2, radius, 0, 360);
+    ctx.fill();
+}
+
+playGame();
+ctx.fillStyle = "red";
 ctx.fillRect(c.clientWidth/2, c.clientHeight/2, 50, 100);
 
 console.log("test")
