77348191855a3cdf4be3b79facb71f50b1f342a7
Yuqing Wu <ywu20@stuy.edu>
2022-03-08 09:57:03 -0500

    moving and generating rectangles

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -6,8 +6,10 @@
 var c = document.getElementById("main_game");
 var ctx = c.getContext("2d");
 var requestID;  //init global var for use with animation frames
-var obstacles = [[200, 0]];
 var radius = 1;
+var levels = [50,150,250];
+var rects = [];
+var generate = true;
 
 var clear = (e) => {
     console.log("clear invoked...")
@@ -21,18 +23,37 @@ var playGame = () => {
     requestID = window.requestAnimationFrame(playGame);
 
     clear();
-    ctx.beginPath();
 
-    // for(var i = 0; i < obstacles.length; i++) {
-    //
-    // }
-    radius += 1;
-    ctx.arc(c.clientWidth/2, c.clientHeight/2, radius, 0, 360);
-    ctx.fill();
+
+
+    if(generate == true){
+    console.log("generating . . .");
+    var start = 300;
+    var num_obs = 10;
+    for (i=0;i<num_obs;i++){
+      var block_len = Math.floor(Math.random() * 100 + 30);
+      var lv = Math.floor(Math.random()*levels.length);
+      start += block_len;
+      rects.push([start,levels[lv], block_len, 20]);
+      //ctx.fillRect(start,levels[lv],block_len, 20);
+    }
+    generate = false;
+  }
+
+  var last_rect = 0;
+  for(i=0;i<rects.length;i++){
+    ctx.fillStyle = "red";
+    ctx.fillRect(rects[i][0],rects[i][1],rects[i][2],rects[i][3]);
+    rects[i][0]-=1;
+    if(rects[i][0] > last_rect){
+      last_rect = rects[i][0];
+    }
+  }
+
+  if(last_rect < 400){
+    generate = true;
+  }
+
 }
 
 playGame();
-ctx.fillStyle = "red";
-ctx.fillRect(c.clientWidth/2, c.clientHeight/2, 50, 100);
-
-console.log("test")
