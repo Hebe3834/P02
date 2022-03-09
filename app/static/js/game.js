d4703de2d27eb3dee100678747c0b936160b76de
Yuqing Wu <ywu20@stuy.edu>
2022-03-09 09:08:06 -0500

    height variation

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -7,7 +7,7 @@ var c = document.getElementById("main_game");
 var ctx = c.getContext("2d");
 var requestID;  //init global var for use with animation frames
 var radius = 1;
-var levels = [50,150,250];
+var levels = [50,200,250];
 var rects = [];
 var generate = true;
 
@@ -25,7 +25,7 @@ var playGame = () => {
     clear();
     //ctx.fillStyle = "white";
     ctx.strokeStyle = "black";
-    ctx.strokeRect(0,levels[1]+50, c.clientWidth, c.clientWidth);
+    ctx.strokeRect(0,levels[1], c.clientWidth, c.clientWidth);
 
     if(generate == true){
     console.log("generating . . .");
@@ -34,10 +34,9 @@ var playGame = () => {
     for (i=0;i<num_obs;i++){
       var block_len = Math.floor(Math.random() * 20 + 10);
       var block_height = Math.floor(Math.random()*80 + 10)
-      var lv = 1;//Math.floor(Math.random()*levels.length);
+      var lv = 1;
       start += block_len + Math.floor(Math.random() * 100) + 50;
-      rects.push([start,levels[lv], block_len, 50]);
-      //ctx.fillRect(start,levels[lv],block_len, 20);
+      rects.push([start,levels[lv] - block_height, block_len, block_height]);
     }
     generate = false;
   }
