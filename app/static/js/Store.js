142e165dba5d1a4f62f1bfca5a7d07bca5d04cc7
Hebe3834 <hebeluna00@gmail.com>
2022-03-23 16:57:40 -0400

    AA

--- app/static/js/Store.js
+++ app/static/js/Store.js
@@ -1,3 +1,8 @@
 var powerSelect = document.getElementById("powerups");
 var powerSelect = document.getElementById("skins");
 powerCarousel = document.getElementById("powerCarousel")
+
+if($('#powerCarousel').length == 0){
+    powerSelect.value = "you have bought everything. how does it feel being rich?";
+    window.alert("test");
+}
